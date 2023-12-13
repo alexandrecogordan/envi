b8060a6d74c61849f7fe6701054309e19cf8df2a
Alexandre Cogordan <alexcogordan2001@hotmail.fr>
2023-12-13 12:03:31 +0100

    Refactor dropdown functionality in app.js and update index.html

--- app.js
+++ app.js
@@ -1,45 +1,29 @@
-$(document).ready(function () {
-    // Load CSV data
-    $.ajax({
-        type: "GET",
-        url: "casino.csv",
-        dataType: "text",
-        success: function (data) {
-            processData(data);
-        }
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const options = dropdown.querySelectorAll('.menu li');
+    const menu = dropdown.querySelector('.menu');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
     });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+        });
+    });
+
 });
-
-function processData(allText) {
-    // Split CSV into rows
-    var allRows = allText.split(/\r?\n|\r/);
-    var tableData = [];
-
-    for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
-        // Split each row into columns
-        var rowCells = allRows[singleRow].split(',');
-
-        // Add data to the table
-        tableData.push(rowCells);
-    }
-
-    // Render the table
-    renderTable(tableData);
-}
-
-function renderTable(data) {
-    var productTable = "<table class='table'><thead><tr><th>Name</th><th>Image Link</th><th>Origin</th><th>Price</th><th>Unit Price</th><th>Bio</th></tr></thead><tbody>";
-
-    for (var i = 1; i < data.length; i++) {
-        productTable += "<tr>";
-        for (var j = 0; j < data[i].length; j++) {
-            productTable += "<td>" + data[i][j] + "</td>";
-        }
-        productTable += "</tr>";
-    }
-
-    productTable += "</tbody></table>";
-
-    // Display the table
-    $("#product-list").html(productTable);
-}
