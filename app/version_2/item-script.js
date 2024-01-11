0e1067e4faed6c738a1039f0e0f62c37f992a401
Alexandre Cogordan <alexcogordan2001@hotmail.fr>
2024-01-11 23:06:24 +0100

    prior to geolocalisation

--- app/version_2/item-script.js
+++ app/version_2/item-script.js
@@ -10,15 +10,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
             if (product) {
                 productDetails.append(`<h1>${product.product_name}</h1>`);
+                productDetails.append(`<img src="${product.image}" alt="${product.product_name}" style="border-radius: 20px;">`);
                 productDetails.append(`<p><strong>🏣 Magasin:</strong> Carrefour</p>`);
                 productDetails.append(`<p><strong>⛰️ Origine:</strong> ${product.origin}</p>`);
-                productDetails.append(`<p><strong>💸 Prix:</strong> ${product.price} €</p>`);
+                
+                if (!product.unit_price) {
+                    product.unit_price = "";
+                }
+                productDetails.append(`<p><strong>💸 Prix:</strong> ${product.price} € (${product.unit_price}) </p>`);
+
                 productDetails.append(`<p><strong>🚚 Distance:</strong> ${product.distance} km</p>`);
+
+                if (product.bio) {
+                    product.bio = "Oui";
+                } else {
+                    product.bio = "Non";
+                }
+                productDetails.append(`<p><strong>🌱 Bio:</strong> ${product.bio}</p>`);
+
+                
             } else {
                 productDetails.append('<p>Product not found</p>');
             }
         });
     } else {
         productDetails.append('<p>No product name provided</p>');
+    }
+
+    function highlightCountryOnMap(countryName) {
+        // Initialize the map if not already initialized
+        // ...
+    
+        var country = geojsonData.features.find(feature => feature.properties.name === countryName);
+    
+        // Highlight the country
+        // This could involve setting a different style for the country's layer
+        if (country) {
+            // Code to highlight the country on the map
+        }
     }
 });
